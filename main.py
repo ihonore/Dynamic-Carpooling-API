b3a63978ead75b3c26be4dd41a6b524c354d3d10
Honore Iradukunda <ihonore01@gmail.com>
2024-06-04 21:54:09 +0200

    adjusted vercel config

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ from dotenv import load_dotenv
 import os
 import uvicorn
 
-
 # Load environment variables from .env file
 load_dotenv()
 
-app=FastAPI()
-
+app = FastAPI()
 app.include_router(api_router)
 
-
 uri = os.getenv("MONGODB_URI")
+if not uri:
+    raise ValueError("MONGODB_URI environment variable is not set")
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
 try:
+    client = MongoClient(uri, server_api=ServerApi('1'))
     client.admin.command('ping')
     print("Pinged. Successfully connected to MongoDB!🟢")
 except Exception as e:
-    print(e)
+    print(f"Error connecting to MongoDB: {e}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
